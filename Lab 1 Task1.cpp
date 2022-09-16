50b8a5385baf2ae496a1de59a491ac265bb80678
Turki-Sh <113216016+Turki-Sh@users.noreply.github.com>
2022-09-16 22:25:32 +0300

    Add files via upload

---
+++ Lab 1 Task1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+	double x,y;
+	cout<<"Please enter x: ";
+	cin>>x;
+	cout<<"Please enter Y: ";
+	cin>>y;
+	cout<<"The result is: "<<x*y;
+	return 0;
+}
